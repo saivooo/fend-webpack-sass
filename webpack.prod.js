a7cde4adf87776a037ff6f794aa4a883bc37d68a
saivooo <isaiah_jennings@yahoo.com>
2023-12-20 21:09:25 -0500

    scss file minimizer implemented

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,14 +20,20 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin()
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin()
     ]
 }
